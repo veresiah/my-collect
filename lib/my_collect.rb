3844d6cfc900a12319fa65ae5403e6b39a2c56c7
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-11-01 14:49:16 -0500

    commit tree ee68f61f874b0fea36ec23e4f39aedba3313ba9f

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+def my_collect(array)
+    i = 0
+    result = []
+    while i < array.length
+      result.push(yield array[i])
+        i += 1
+    end
+    result
+  end
+  
+  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  my_collect(array) do |name|
+    name.split(" ").first
+  end
 
